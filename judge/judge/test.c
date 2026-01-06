3d84362aff024465c2d22363349c2316abec2b6b
man <3199347595@qq.com>
2026-01-06 23:44:19 +0800

    用联合体判断大小端

--- judge/judge/test.c
+++ judge/judge/test.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
+//int main() {
+//	int a = 1;
+//	if(*(char*)&a== 1) {
+//		printf("是小端\n");
+//	} else {
+//		printf("是大端\n");
+//	}
+//}
+// 
+// 
+//用联合体实现大端小端判断
+int check_sys() {
+	union Un{
+		int a;
+		char b;
+	} c;
+	c.a = 1;
+	return c.b;
+}
 int main() {
-	int a = 1;
-	if(*(char*)&a== 1) {
+	if(check_sys() == 1) {
 		printf("是小端\n");
 	} else {
 		printf("是大端\n");
