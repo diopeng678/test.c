481b72fad6c8e9dc69927695768d3b8c281028f4
man <3199347595@qq.com>
2026-01-14 23:05:27 +0800

    依旧

--- 文件操作/文件操作/FILE.c
+++ 文件操作/文件操作/FILE.c
@@ -2,25 +2,67 @@
 #include<stdio.h>
 
 
+//int main() {
+//	FILE *fp;
+//	char str[100];
+//	// 打开文件以读取
+//	fp = fopen("6666", "w");
+//	if (fp == NULL) {
+//		perror("无法打开文件");
+//		return -1;
+//	}
+//		char mmo = 0;
+//		for (mmo = 'a';mmo <= 'z'; mmo++)
+//		{
+//			fputc(mmo, fp);
+//			
+//		}
+//		fputc(97, fp);
+//		//以ASCII码形式写入字符的形式进行计入的
+//
+//		// 关闭文件
+//	fclose(fp);
+//	return 0;
+//}
+
+//int main() {
+//	FILE* fp;
+//	char str[100];
+//	 打开文件以读取
+//	fp = fopen("6666", "r");
+//	if (fp == NULL) {
+//		perror("无法打开文件");
+//		return -1;
+//	}
+//		int ch = fgetc(fp);
+//		printf("%c\n", ch);
+//
+//		 ch = fgetc(fp);
+//		printf("%c\n", ch);
+//
+//		 ch = fgetc(fp);
+//		printf("%c\n",ch );
+//
+//		进行读取的时候会将光标向后移动
+//
+//	fclose(fp);
+//	return 0;
+//}
+
 int main() {
-	FILE *fp;
+	FILE* fp;
 	char str[100];
-	// 打开文件以读取
-	fp = fopen("6666", "w");
+	// 打开文件以读取,用循环的方式去读取文件中的内容,就是一下子读取完
+	fp = fopen("6666", "r");
 	if (fp == NULL) {
 		perror("无法打开文件");
 		return -1;
 	}
-		char mmo = 0;
-		for (mmo = 'a';mmo <= 'z'; mmo++)
-		{
-			fputc(mmo, fp);
-			
-		}
-		fputc(97, fp);
-		//以ASCII码形式写入字符的形式进行计入的
-
-		// 关闭文件
+	int ch=0;
+	while(( ch = fgetc(fp)) != EOF)
+	{
+		printf("%c\n", ch);
+	}	
 	fclose(fp);
 	return 0;
 }
