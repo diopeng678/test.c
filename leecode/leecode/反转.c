53be25dfec30cf97606aaa4a84ddcbbf9a94e728
man <3199347595@qq.com>
2026-02-03 17:47:56 +0800

    依旧刷题 leecode 138

--- leecode/leecode/反转.c
+++ leecode/leecode/反转.c
@@ -1,20 +1,73 @@
 #define _CRT_SECURE_NO_WARNINGS_1
-void reverse(int* a, int left, int right) {
-    while (left < right) {
-        int tmp = a[left];
-        a[left] = a[right];
-        a[right] = tmp;
-        left++;
-        right--;
+//void reverse(int* a, int left, int right) {
+//    while (left < right) {
+//        int tmp = a[left];
+//        a[left] = a[right];
+//        a[right] = tmp;
+//        left++;
+//        right--;
+//
+//    }
+//
+//
+//}
+//
+//void rotate(int* nums, int numsSize, int k) {
+//    k %= numsSize;
+//    reverse(nums, 0, numsSize - k - 1);
+//    reverse(nums, numsSize - k, numsSize - 1);
+//    reverse(nums, 0, numsSize - 1);
+//}
+//
+
+
+//leetcode 138. 复制带随机指针的链表
+struct Node* copyRandomList(struct Node* head) {
+    //拷贝节点差入到源节点的后面
+
+    struct Node* cur = head;
+    while (cur) {
+        struct Node* copy = (struct Node*)malloc(sizeof(struct Node));
+        copy->val = cur->val;
+
+        copy->next = cur->next;
+        cur->next = copy;
+
+        cur = copy->next;
 
     }
+    //控制random
+    cur = head;
+    while (cur)
+    {
+        struct Node* copy = cur->next;
+        if (cur->random == NULL) {
+            copy->random = NULL;
+        }
+        else {
+            copy->random = cur->random->next;
 
+        }
+        cur = copy->next;
 
-}
+    }
+    //把拷贝下来的节点取下来尾插成新的链表,然后恢复原链表(不恢复也行)
+    struct Node* copyhead = NULL, * copytail = NULL;
+    cur = head;
+    while (cur)
+    {
+        struct Node* copy = cur->next;
+        struct Node* next = copy->next;
 
-void rotate(int* nums, int numsSize, int k) {
-    k %= numsSize;
-    reverse(nums, 0, numsSize - k - 1);
-    reverse(nums, numsSize - k, numsSize - 1);
-    reverse(nums, 0, numsSize - 1);
+        if (copytail == NULL)
+        {
+            copyhead = copytail = copy;
+        }
+        else {
+            copytail->next = copy;
+            copytail = copytail->next;
+        }
+        cur = next;
+    }
+    return copyhead;
 }
