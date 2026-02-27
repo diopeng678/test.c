f17c9503b4582c273400c4ed1ac1f8922a11399a
man <3199347595@qq.com>
2026-02-27 21:20:29 +0800

    刷题

--- leecode/leecode/反转.c
+++ leecode/leecode/反转.c
@@ -100,3 +100,31 @@ struct Node* copyRandomList(struct Node* head) {
 //
 //    return a;
 //}
+
+//572. 另一个树的子树
+//bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+//    if (p == NULL && q == NULL)
+//        return true;
+//
+//    if (p == NULL || q == NULL)
+//        return false;
+//
+//    if (p->val != q->val)
+//        return false;
+//
+//    return isSameTree(p->left, q->left)
+//        && isSameTree(p->right, q->right);
+//}
+//
+//bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot) {
+//    if (root == NULL)
+//        return false;
+//
+//    if (root->val == subRoot->val
+//        && isSameTree(root, subRoot))
+//        return true;
+//
+//    return isSubtree(root->left, subRoot)
+//        || isSubtree(root->right, subRoot);
+//
+//}
