d3ad8b5ce6043bb9298776fa203422c9411875f1
man <3199347595@qq.com>
2026-03-19 17:57:41 +0800

    运算符重载

--- C++/C++/first.cpp
+++ C++/C++/first.cpp
@@ -21,6 +21,12 @@ void A() {
 	cout << "全局函数" << std::endl;
 }
 
+//运算符重载
+bool operator == (const C& c1, const C& c2) {
+	return true; //简单返回true，表示两个对象相等
+}
+
+
 //空类的大小为1字节
 class man{};
 
@@ -35,5 +41,7 @@ int main() {
 	cout <<" 这是全局变量 "<< ::a << endl; //全局变量  10
 	cout << "常量引用: " << &c <<"  "<< & h << endl; //常量引用  40
 	//引用的两个数是一样的，说明引用就是一个别名
+
+
 	return 0;
 }
