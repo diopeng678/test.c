3afef0fe6eb944d22332931b1420bfb65c728358
man <3199347595@qq.com>
2026-04-02 11:53:30 +0800

    复写零 1083

--- leetcode/leecode/反转.c
+++ leetcode/leecode/反转.c
@@ -153,3 +153,36 @@ struct Node* copyRandomList(struct Node* head) {
 //    reverse(nums, 0, numsSize - 1);
 //}
 //
+// ∏¥–¥¡„leetcode 1089
+//class Solution {
+//public:
+//    void duplicateZeros(vector<int>& arr) {
+//        int dest = -1, cur = 0, n = arr.size();
+//        while (cur < n)
+//        {
+//            if (arr[cur]) dest++;
+//            else dest += 2;
+//            if (dest >= n - 1) break;
+//            cur++;
+//        }
+//
+//
+//        if (dest == n)
+//        {
+//            arr[n - 1] = 0;
+//            cur--; dest -= 2;
+//        }
+//
+//
+//        while (cur >= 0)
+//        {
+//            if (arr[cur]) arr[dest--] = arr[cur--];
+//            else
+//            {
+//                arr[dest--] = 0;
+//                arr[dest--] = 0;
+//                cur--;
+//            }
+//        }
+//    }
+//};
