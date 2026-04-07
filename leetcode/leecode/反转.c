9f22b24b80274f863eada16bca02c9b63338d74f
man <3199347595@qq.com>
2026-04-07 13:32:46 +0800

    窗口滑动解决问题

--- leetcode/leecode/反转.c
+++ leetcode/leecode/反转.c
@@ -309,3 +309,20 @@ struct Node* copyRandomList(struct Node* head) {
 //    }
 //
 //};
+//209 ´°¿Ú»¬¶¯ 
+//class Solution {
+//public:
+//    int minSubArrayLen(int target, vector<int>& nums) {
+//        int n = nums.size(), sum = 0, len = INT_MAX;
+//        for (int left = 0, right = 0; right < n; right++)
+//        {
+//            sum += nums[right];
+//            while (sum >= target)
+//            {
+//                len = min(len, right - left + 1);
+//                sum -= nums[left++];
+//            }
+//        }
+//        return len == INT_MAX ? 0 : len;
+//    }
+//};
