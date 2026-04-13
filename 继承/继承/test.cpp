5075a9ea3e3ee141f302ee44032292aadb44fca6
man <3199347595@qq.com>
2026-04-13 09:52:06 +0800

    继承

--- 继承/继承/test.cpp
+++ 继承/继承/test.cpp
@@ -64,6 +64,59 @@ private:
 	int sex = 9;
 };
 
+class person
+{
+public:
+	person(int a = 19, string n = "jiege", string s = "nan")
+		: year(a)
+		, name(n)
+		, sex(s)
+	{ }
+	person(const person& p)
+		: year(p.year)
+		, name(p.name)
+		, sex(p.sex)
+	{ }
+	void print()
+	{
+		cout << "year: " << year << " name: " << name << " sex: " << sex << endl;
+	}
+
+private:
+	int year = 18;
+	string name = "niao";
+	string sex = "nan";
+};
+
+class student :public person
+{
+public:
+	student(int a = 666, string n ="matou", string s = "jige", int num = 99, string add = "beijing")
+		: person(a, n, s)
+		, _num(num)
+		, _add(add)
+	{
+
+	}
+	
+	student(const student& s)
+		: person(s)
+		, _num(s._num)
+		, _add(s._add)
+	{
+
+	}
+	void print()
+	{
+		person::print();
+		cout << " num: " << _num << " add: " << _add << endl;
+	}
+private:
+	int _num;
+	string _add;
+};
+
+
 int main() 
 {
 		man m;
@@ -76,6 +129,14 @@ int main()
 		k1.print();
 		kobe k2(999,777);
 		k2.print();
+		/*person p1(20, "shipeng", "nv");
+		person p2;
+		p2.print();*/
+
+		student s1(666, "matou", "mmmmmmm", 99, "beijing");
+		s1.print();
+
+
 		return 0;
 		
 }
