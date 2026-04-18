ca1d40a495824fdd768cc842dfd3dd5a5e7f3444
man <3199347595@qq.com>
2026-04-18 23:19:34 +0800

    算法

--- leetcode/leecode/反转.c
+++ leetcode/leecode/反转.c
@@ -532,3 +532,35 @@
 //        return ret;
 //    }
 //};
+
+//30 串联所有单词的子串
+//class Solution {
+//public:
+//    vector<int> findSubstring(string s, vector<string>& words) {
+//        vector<int> ret;
+//        unordered_map<string, int> hash1;
+//        for (auto& s : words) hash1[s]++;
+//        int len = words[0].size();
+//        int m = words.size();
+//        for (int i = 0;i < len;i++)
+//        {
+//            unordered_map<string, int> hash2;
+//            for (int left = i, right = i, count = 0;right + len <= s.size(); right += len)
+//            {
+//                string in = s.substr(right, len);
+//                hash2[in]++;
+//                if (hash1.count(in) && hash2[in] <= hash1[in]) count++;
+//                if (right - left + len > len * m)
+//                {
+//                    string out = s.substr(left, len);
+//                    if (hash1.count(out) && hash2[out] <= hash1[out]) count--;
+//                    hash2[out]--;
+//                    left += len;
+//                }
+//                if (count == m) ret.push_back(left);
+//            }
+//
+//        }
+//        return ret;
+//    }
+//};
