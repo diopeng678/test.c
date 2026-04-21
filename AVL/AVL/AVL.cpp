d4276c6c9c183b28b96f4c1a87a55a6d0fa4924b
man <3199347595@qq.com>
2026-04-21 19:30:05 +0800

    AVL的插入 真有点实力了 难到爆

--- AVL/AVL/AVL.cpp
+++ AVL/AVL/AVL.cpp
@@ -1,4 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include<map>
+#include<assert.h>
+using namespace std;
 
 template<class K, class V>
 struct AVLTreeNode
@@ -16,6 +20,7 @@ struct AVLTreeNode
         , _parent(nullptr)
         , _bf(0)
     {
+
     }
 };
 
@@ -23,9 +28,98 @@ template<class K, class V>
 class AVLTree
 {
     typedef AVLTreeNode<K, V> Node;
+
 private:
+
     Node* _root = nullptr;
 public:
-    bool Insert(const pair<K, V>& kv);
-    // 其他接口下篇实现
+    //插入接口
+    bool Insert(const pair<K, V>& kv)
+    {
+        //判断是否为空
+        if (_root == nullptr)
+        {
+            _root = new Node(kv);
+            return true;
+        }
+        Node* parent = nullptr;
+        Node* cur = _root;
+		//找到插入位置 确定父节点
+        while (cur)
+        {
+            if (kv.first < cur->_kv.first)
+            {
+                parent = cur;
+                cur = cur->_left;
+            }
+			else if (kv.first > cur->_kv.first)
+            {
+                parent = cur;
+                cur = cur->_right;
+            }
+            else
+            {
+                return false; //不允许插入重复的key
+            }
+        }
+		//将新节点插入到父节点的左子树或右子树
+        cur = new Node(kv);
+        if ( parent->_kv.first < kv.first)
+        {
+            parent->_right= cur;
+        }
+        else
+        {
+            parent->_left = cur;
+        }
+        cur->_parent = parent;
+
+		//更新平衡因子
+        while (parent)
+        {
+            if (cur == parent->_left)
+            {
+                parent->_bf++;
+            }
+            else
+            {
+                parent->_bf--;
+            }
+
+            if (parent->_bf == 0)
+            {
+                break;
+            }
+
+            if (parent->_bf == 1 || parent->_bf == -1)
+            {
+                cur = parent;
+                parent = parent->_parent;
+            }
+            else if (parent->_bf == 2 || parent->_bf == -2)
+            {
+                break;
+            }
+            else
+            {
+                assert(false);
+            }
+        }
+
+        return true;
+        
+    }
 };
+//我操了 这么难
+
+int main()
+{
+    AVLTree<int, int> avl;
+    avl.Insert(make_pair(1, 1));
+    avl.Insert(make_pair(2, 2));
+    avl.Insert(make_pair(3, 3));
+    avl.Insert(make_pair(4, 4));
+    avl.Insert(make_pair(5, 5));
+    
+    return 0;
+}
