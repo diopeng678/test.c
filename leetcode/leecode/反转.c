e5d5fbe4b0191b09f163455bfb7dd6f342409903
man <3199347595@qq.com>
2026-04-28 22:54:50 +0800

    二分查找 基础

--- leetcode/leecode/反转.c
+++ leetcode/leecode/反转.c
@@ -564,3 +564,20 @@
 //        return ret;
 //    }
 //};
+
+//  704 二分查找
+//class Solution {
+//public:
+//    int search(vector<int>& nums, int target) {
+//        int left = 0;
+//        int right = nums.size() - 1;
+//        while (left <= right)
+//        {
+//            int mid = left + (right - left) / 2;
+//            if (nums[mid] < target) left = mid + 1;
+//            else if (nums[mid] > target) right = mid - 1;
+//            else return mid;
+//        }
+//        return -1;
+//    }
+//};
